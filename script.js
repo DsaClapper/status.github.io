72fdda4b1e639cbb18174735415f85cd51f6b84b
DsaClapper <71221540+DsaClapper@users.noreply.github.com>
2025-03-19 16:17:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ async function getServerStatus(serverId) {
     // Simulate server status data (replace with your actual data fetching method)
 
     //fetch Server 1
-    if(serverId === "mikenug") {
+    if (serverId === "mikenug") {
         const status = fetch(`https://mikenug-server-new.onrender.com/`)
-        .then((response) => response.text())
-        .then(async (status) => {
-            if(status === "Servers Up") {
-                return "Online";
-            }
-        }).catch(e => {
-            return "Offline"
-        })
+            .then((response) => response.text())
+            .then(async (status) => {
+                if (status === "Servers Up") {
+                    return "Online";
+                }
+            }).catch(e => {
+                return "Offline"
+            })
 
         return {
             status: await status
         };
-    }else if(serverId === "halalas-cloud") {
+    } else if (serverId === "halalas-cloud") {
         const status = fetch(`https://data.halalas-services.xyz/`)
-        .then((response) => response.text())
-        .then(async (status) => {
-            if(status === "Servers Up") {
-                return "Online";
-            }
-        }).catch(e => {
-            return "Offline";
-        });
+            .then((response) => response.text())
+            .then(async (status) => {
+                if (status === "Servers Up") {
+                    return "Online";
+                }
+            }).catch(e => {
+                return "Offline";
+            });
 
         return {
             status: await status
@@ -53,3 +53,33 @@ async function updateStatus() {
 // Update status every 10 seconds
 setInterval(updateStatus, 10000);
 updateStatus(); // Initial update when the page loads
+
+//Theme change
+
+document.getElementById("theme-button").addEventListener('click', () => {
+    themeChange();
+}, false);
+
+function themeChange() {
+    document.getElementById("sunmoon").classList.toggle('dark');
+    document.querySelector("body").classList.toggle("dark");
+    document.querySelectorAll(".server-status h2").forEach(e => {
+e.classList.toggle("dark");
+    })
+    document.querySelectorAll(".server-status p").forEach(e => {
+        e.classList.toggle("dark");
+            })
+    document.querySelector("h1").classList.toggle("dark")
+    document.querySelectorAll(".server-status").forEach(e => {
+        e.classList.toggle("dark")
+    });
+    let theme = document.querySelector("h1").classList.contains("dark")? "dark" : "light";
+    localStorage.setItem("theme-preference", theme)
+}
+
+window.onload = themeLoad
+function themeLoad() {
+    if(localStorage.getItem("theme-preference") === "dark") {
+        themeChange();
+    }
+}
